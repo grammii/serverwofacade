eef3906eebee2c2cd4265d0843a511d105890d4a
sornpat2 <sornpat@gmail.com>
2022-09-04 22:50:51 +0700

    Final

---
+++ src/serverwofacade/FacadeDesignPattern.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverwofacade;
+
+/**
+ *
+ * @author sornp
+ */
+public class FacadeDesignPattern {
+    public void start(){
+        ScheduleServer obj1 = new ScheduleServer();
+        obj1.startBooting();
+        obj1.readSystemConfigFile();
+        obj1.init();
+        obj1.initializeContext();
+        obj1.initializeListeners();
+        obj1.createSystemObjects();
+        System.out.println("Start working......");
+    }
+    
+    public void stop(){
+        ScheduleServer obj2 = new ScheduleServer();
+        System.out.println("After work done.........");
+        obj2.releaseProcesses();
+	obj2.destory();
+	obj2.destroySystemObjects();
+	obj2.destoryListeners();
+	obj2.destoryContext();
+	obj2.shutdown();
+    }
+}
